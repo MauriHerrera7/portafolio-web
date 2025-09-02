b546f08e4bf7d9621b748f089f373978449cfab1
MauriHerrera7 <mauriherrera457@gmail.com>
2025-09-02 20:06:20 -0300

    cambios

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,12 +3,6 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  // Forzar modo oscuro siempre al montar
-  if (typeof window !== 'undefined') {
-    localStorage.setItem('theme', 'dark');
-    document.documentElement.classList.add('dark');
-    document.documentElement.classList.remove('light');
-  }
   return (
     <ThemeProvider 
       attribute="class" 
